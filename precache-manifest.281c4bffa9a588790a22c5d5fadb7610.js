994112ece9b07061d558256377e39769b1b6ae0f
Ranjith <ranjithsiva2211@gmail.com>
2020-07-26 19:37:25 +0530

    Updates

---
+++ precache-manifest.281c4bffa9a588790a22c5d5fadb7610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9632b076201ad77211ec82219b5125",
+    "url": "/bookingapp/index.html"
+  },
+  {
+    "revision": "8476e660b291e2dfe380",
+    "url": "/bookingapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "55745ad49545de35aa81",
+    "url": "/bookingapp/static/js/2.7489c3cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bookingapp/static/js/2.7489c3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8476e660b291e2dfe380",
+    "url": "/bookingapp/static/js/main.feb0dbe2.chunk.js"
+  },
+  {
+    "revision": "68503ab37b25bef0ebdf",
+    "url": "/bookingapp/static/js/runtime-main.4c0a09a2.js"
+  }
+]);
