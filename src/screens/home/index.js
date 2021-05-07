4d23629491581bb55abf02b5f336c4150279a71b
RanjithSiva22 <ranjithsiva2211@gmail.com>
2021-05-07 09:20:26 +0530

    api commit

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Carousel from '../../components/carousel/index';
 import Head from '../../components/header/index';
 import CustomCard from '../../components/Card/index';
@@ -8,32 +8,32 @@ import Footer from '../../components/footer/index';
 
 
 function Home() {
-    const [film,setFilm]=useState([]);
-    
-    useEffect(()=>{
+    const [film, setFilm] = useState([]);
+
+    useEffect(() => {
         // const t='';
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=a45396cacc99a4038a86636629609de1&language=en-US&page=1`)
-        .then(res=>res.json())
-        .then(res=>{
-            const t=res.results;
-           
-            console.log(t);
-            setFilm(t);
-            
-        })
-    },[]);
+            .then(res => res.json())
+            .then(res => {
+                const t = res.results;
+
+                console.log(t);
+                setFilm(t);
+
+            })
+    }, []);
     return (
         <div>
-            <Head/>
+            <Head />
             {console.log(film[0])}
-            <div style={{marginTop:"4rem",marginBottom:"1rem"}}>
-            <Carousel />
+            <div style={{ marginTop: "4rem", marginBottom: "1rem" }}>
+                <Carousel />
             </div>
             <h1>POPULAR MOVIES</h1>
             <div className="row">
                 {film.map(item => <CustomCard key={item.id} item={item} />)}
             </div>
-            <Footer/>
+            <Footer />
 
         </div>
     )
